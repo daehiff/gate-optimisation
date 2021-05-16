3712cc176c64c1d2bec9659fe1278a81020ee78f
daehiff <winderl13@gmail.com>
2021-05-16 18:57:28 +0200

    save the state

--- plot.py
+++ plot.py
@@ -13,15 +13,35 @@ def main():
     plot_default()
 
 
-def plot_default():
+def plot_default(include_qfast=False):
+    df = pd.read_csv("data/default/gate_count.csv")
+    plot_by_metric(df, metric="circ_num_gates", include_qfast=include_qfast)
+
+    df = pd.read_csv("data/default/num_qubits.csv")
+    plot_by_metric(df, metric="circ_q_count", include_qfast=include_qfast)
+
+    df = pd.read_csv("data/default/cnot_pr.csv")
+    plot_by_metric(df, metric="p_cnot", include_qfast=include_qfast)
+
+    df = pd.read_csv("data/default/t_pr.csv")
+    plot_by_metric(df, metric="p_t", include_qfast=include_qfast)
+
+
+def plot_default(include_qfast=False):
     df = pd.read_csv("data/rebase/gate_count.csv")
-    plot_by_metric(df, metric="circ_num_gates")
+    plot_by_metric(df, metric="circ_num_gates", include_qfast=include_qfast)
 
     df = pd.read_csv("data/rebase/num_qubits.csv")
-    plot_by_metric(df, metric="circ_q_count")
+    plot_by_metric(df, metric="circ_q_count", include_qfast=include_qfast)
+
+    df = pd.read_csv("data/rebase/cnot_pr.csv")
+    plot_by_metric(df, metric="p_cnot", include_qfast=include_qfast)
+
+    df = pd.read_csv("data/rebase/t_pr.csv")
+    plot_by_metric(df, metric="p_t", include_qfast=include_qfast)
 
 
-def plot_by_metric(df, metric="circ_num_gates"):
+def plot_by_metric(df, metric="circ_num_gates", include_qfast=True):
     df_default = df[df["algorithm"].str.match("default")]
     df_default = pd.DataFrame(np.repeat(df_default.values, 7, axis=0), columns=df_default.columns)
     del df_default["Unnamed: 0"]
@@ -37,10 +57,11 @@ def plot_by_metric(df, metric="circ_num_gates"):
     df_["algorithm"] = alg
     df_[metric] = metric_
     df_ = df_[~df_["algorithm"].str.match("default")]
-    df_ = df_[~df_["algorithm"].str.match("qfast")]
+    if not include_qfast:
+        df_ = df_[~df_["algorithm"].str.match("qfast")]
     for comparison_column in COMPARISONS:
         sns.barplot(x=metric, hue="algorithm", y=comparison_column, data=df_, errwidth=1, capsize=0.1)
-        plt.title(f"Comparison against: {comparison_column}")
+        plt.title(f"Metric: {comparison_column}")
         plt.show()
 
 
